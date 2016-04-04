b3b5c38624b4b7612412a729a58687e84abf37f0
linjie <linjie@handlecar.com>
2016-04-04 21:20:57 +0800

    no message

---
+++ nodeWeb/server/dao/testConfig.js
@@ -0,0 +1,29 @@
+var mysql         = require('mysql');        
+var TEST_DATABASE = 'node1s';  
+var TEST_TABLE    = 'nuser';  
+//创建连接  
+var client        = mysql.createConnection({  
+  user     : 'root',  
+  password : 'root',  
+});  
+
+client.connect();
+client.query("use " + TEST_DATABASE);
+
+client.query(  
+  'SELECT * FROM '+TEST_TABLE,  
+  function selectCb(err, results, fields) {  
+    if (err) {  
+      throw err;  
+    }  
+      
+      if(results)
+      {
+          for(var i = 0; i < results.length; i++)
+          {
+              console.log("%d\t%s\t%s", results[i].nuserid, results[i].nusername, results[i].nusertel);
+          }
+      }    
+    client.end();  
+  }  
+); 
