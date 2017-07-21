1fc277b513db5eb5710c311eb0ec2fcffbe6f54f
j.lin <j.lin@baisonmail.com>
2017-07-21 18:09:24 +0800

    添加推箱子game

---
+++ gameProject/js/boxit.js
@@ -0,0 +1,223 @@
+var app = angular.module('app', []);
+app.controller('appController', ['$scope', '$timeout', ($scope, $timeout) => {
+	$scope.data = {
+        grade: 3,
+        xMax: grades.grade3.defaultTable.length,
+        yMax: grades.grade3.defaultTable[0].length,
+        // 0: empty, 1: wall, 2: empty backgroud, 3: target, 4: empty box, 5: reach
+        gradeTable: grades.grade3.defaultTable,
+        protagonist: grades.grade3.protagonist,
+        nowCount: 0
+	}
+
+	$scope.keyEvent = (e) => {
+		if(!e || e.keyCode!=37 && e.keyCode!=38 && e.keyCode!=39 && e.keyCode!=40 && e.keyCode!=65 && e.keyCode!=68 && e.keyCode!=83 && e.keyCode!=87) return;
+		// console.log(e.keyCode)
+        switch (e.keyCode) {
+            case 37: 
+            case 65: 
+                // console.log("left")
+                if($scope.data.protagonist.x==0 || $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1]==1) break;
+                if($scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1]==4) {
+                    // console.log("左侧有空箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==2) {
+                        // console.log("空箱子往左移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==3) {
+                        // console.log("空箱子往左移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2] = 5;
+                    }
+                }
+                if($scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1]==5) {
+                    // console.log("左侧有reach箱子");
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==1) {
+                        // console.log("reach箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==2) {
+                        // console.log("reach箱子往左移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2]==3) {
+                        // console.log("reach箱子往左移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-1] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x-2] = 5;
+                    }
+                }
+                $scope.data.protagonist.x -= 1;
+                break;
+            case 38:
+            case 87:
+                // console.log("top")
+                if($scope.data.protagonist.y==0 || $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x]==1) break;
+                if($scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] == 4) {
+                    // console.log("上面有空箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==2) {
+                        // console.log("空箱子往上移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==3) {
+                        // console.log("空箱子往上移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x] = 5;
+                    }
+                }
+                if( $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] == 5) {
+                    // console.log("上面有reach箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==2) {
+                        // console.log("reach箱子往上移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x]==3) {
+                        // console.log("reach箱子往上移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y-1][$scope.data.protagonist.x] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y-2][$scope.data.protagonist.x] = 5;
+                    }
+                }
+                $scope.data.protagonist.y -= 1;
+                break;
+            case 39:
+            case 68:
+                // console.log("right")
+                if($scope.data.protagonist.x==($scope.data.xMax-1) || $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1]==1) break;
+                if($scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] == 4) {
+                    // console.log("右面有空箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==2) {
+                        // console.log("空箱子往右移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==3) {
+                        // console.log("空箱子往右移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2] = 5;
+                    }
+                }
+                if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] == 5) {
+                    // console.log("右面有reach箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==2) {
+                        // console.log("reach箱子往右移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2]==3) {
+                        // console.log("reach箱子往右移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+1] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y][$scope.data.protagonist.x+2] = 5;
+                    }
+                }
+                $scope.data.protagonist.x += 1;
+                break;
+            case 40:
+            case 83:
+                // console.log("bottom")
+                if($scope.data.protagonist.y==($scope.data.yMax-1) || $scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x]==1) break;
+                if($scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x]==4) {
+                    // console.log("下面有空箱子");
+
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==1) {
+                        // console.log("空箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==2) {
+                        // console.log("空箱子往下移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==3) {
+                        // console.log("空箱子往下移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x] = 2;
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x] = 5;
+                    }
+                }
+                if($scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x]==5) {
+                    // console.log("下侧有reach箱子");
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==4 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==5 ||
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==1) {
+                        // console.log("reach箱子前面有障碍");
+                        break;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==2) {
+                        // console.log("reach箱子往下移一格");
+                        $scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x] = 4;
+                    }
+                    if( $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x]==3) {
+                        // console.log("reach箱子往下移一格 reach");
+                        $scope.data.gradeTable[$scope.data.protagonist.y+1][$scope.data.protagonist.x] = 3;
+                        $scope.data.gradeTable[$scope.data.protagonist.y+2][$scope.data.protagonist.x] = 5;
+                    }
+                }
+                $scope.data.protagonist.y += 1;
+                break;
+
+        }
+        $timeout(() => {
+            $scope.checkPass();
+        })
+	}
+
+    $scope.checkPass = () => {
+        var tempGradeName = "grade" + $scope.data.grade;
+        $scope.data.nowCount = document.querySelectorAll(".reach").length;
+
+        if($scope.data.nowCount == grades[tempGradeName].passCount) {
+            $scope.data.grade += 1;
+            tempGradeName = "grade" + $scope.data.grade;
+            $scope.data = {
+                grade: $scope.data.grade,
+                xMax: grades[tempGradeName].defaultTable.length,
+                yMax: grades[tempGradeName].defaultTable[0].length,
+                gradeTable: grades[tempGradeName].defaultTable,
+                protagonist: grades[tempGradeName].protagonist,
+                nowCount: 0
+            }
+        }
+    }
+}]);
